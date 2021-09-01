a35699a47cf8beb6966a652f4b607f7695d5911e
Lizzy Wiley <Lizzyrho@gmail.com>
2021-09-01 08:49:24 -0500

    added reset button

--- maintwo.js
+++ maintwo.js
@@ -18,9 +18,11 @@ console.log("Sanity") // sanity
 let ourString = "Code School is a hands-on class designed to train individuals to be entry-level software developers within a six-month course. The minimum education requirement is at least a high school diploma or GED. Students work in a small classroom setting and use real-word technologies to learn the fundamentals of coding, app development, the leadership, and the life skills needed for a successful career and job market competitiveness.";
 
 // 1. GET A REFERENCE TO THE DOM!
+let reset = document.getElementById('RESET');
 let textinput = document.getElementById('TEXTINPUT');
 let output = document.getElementById('USEROUTPUT');
 let index = 0 // set the index of the array to equal 0
+let newWord;
 // 2. We need to attach the event we want something to happen to with an event listener
 textinput.addEventListener("keydown", (event) => {
     console.log(`${event.key} is pressed`);// debugger
@@ -31,7 +33,20 @@ textinput.addEventListener("keydown", (event) => {
     let split = ourString.split(""); // we split the string we want to use one character at a time into a seperate array
     console.log(split); //debugger
     usertype = split; //any word the user types is going to be replaced with this string!
-    let newWord = split[index]; // we store the results of the string's index
+    newWord = split[index]; // we store the results of the string's index
     console.log(newWord); // debugger
     output.append(newWord); // append it to show up in our P element!!
 })  
+
+// TODO: ### Exercise 3:
+// Enhance your solution by creating a reset button that will erase everything from the label so the user can start over.
+
+reset.addEventListener("click", () => {
+     let empty = " "; // set a variable to null or empty
+     output.textContent = empty; // replace the text content to the null variable
+     console.log(empty); //debugger!
+    
+   
+    
+    
+});
